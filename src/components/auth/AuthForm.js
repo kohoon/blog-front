d3946daa7efd2c815d4fc7b0d85790d884fb97c1
kohoon <kohoon0140@gmail.com>
2020-11-26 02:20:30 +0900

    first commit

---
+++ src/components/auth/AuthForm.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components';
+import { Link } from 'react-router-dom'
+import palette from '../../lib/styles/palette'
+import Button from '../common/Button'
+
+// 회원가입 또는 로그인 폼을 보여줍니다.
+
+const AuthFormBlock = styled.div`
+`;
+
+const AuthForm = () => {
+    return (
+        <AuthFormBlock>
+            AuthForm
+        </AuthFormBlock>
+    );
+};
+
+export default AuthForm;
